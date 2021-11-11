c9d483600f0b775f2987a2dc1cb9cb606e9a29a9
Eze Gatica <ezequielgatica@gmail.com>
2021-11-11 10:20:30 -0300

    jueguito de mas rapido tocar

--- app/src/main/java/app/gati/HomeScreen.java
+++ app/src/main/java/app/gati/HomeScreen.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 public class HomeScreen extends AppCompatActivity {
     TextView timerTxt;
     TextView textito;
+    TextView textoBestTime;
+
     Button botonEmpezar;
     int estado;
     //#Estados:
@@ -18,17 +20,27 @@ public class HomeScreen extends AppCompatActivity {
     // Stop:     1
     // Reinciar: 2
     private long startTime;
-
+    long millis;
+    long millisbien;
+    int seconds;
+    int minutes;
+    long bestTime = 0;
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
         @Override
         public void run() {
-            long millis = System.currentTimeMillis() - startTime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
+            millis = System.currentTimeMillis() - startTime;
+            if (millis == 0)
+                return;
+            millisbien = millis;
+            if (millis > 1000)
+                millisbien = millis % (int) Math.pow(10, (int) Math.log10(millis));
+            //System.out.println(millisbien);
+            seconds = (int) (millis / 1000);
+            minutes = seconds / 60;
             seconds = seconds % 60;
-            timerTxt.setText(String.format("%d:%02d:%02d", minutes, seconds,millis));
-            timerHandler.postDelayed(this, 100);
+            timerTxt.setText(String.format("%d:%02d:%02d", minutes, seconds, millisbien));
+            timerHandler.postDelayed(this, 5);
         }
     };
 
@@ -40,6 +52,7 @@ public class HomeScreen extends AppCompatActivity {
         botonEmpezar.setText(getString(R.string.empezar));
         textito =  findViewById(R.id.textito);
         timerTxt = findViewById(R.id.textoTimer);
+        textoBestTime = findViewById(R.id.bestTime);
         estado = 0;
 
         textito.setText(getString(R.string.saludo, getIntent().getStringExtra(Login.getMESSAGE())));
@@ -51,6 +64,12 @@ public class HomeScreen extends AppCompatActivity {
                 if (estado == 1) {
                     timerHandler.removeCallbacks(timerRunnable);
                     botonEmpezar.setText(getString(R.string.reiniciar));
+                    System.out.println(millis);
+                    if (bestTime > millis || bestTime == 0) {
+                        bestTime = millis;
+                        textoBestTime.setText(String.format("%d ms",millis));
+                    }
+
                     estado = 2;
                 } else {
                     startTime = System.currentTimeMillis();
